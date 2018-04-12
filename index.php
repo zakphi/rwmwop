ef5998c36e8f0aea19ecb6dbe3ae9b2a942cc73c
Philip Zak <zakphi@gmail.com>
2018-04-12 16:27:24 -0400

    fix code alignment

--- index.php
+++ index.php
@@ -20,36 +20,36 @@
     <?php $query = new WP_Query( array('post_type' => 'featured_artwork') ); ?>
     <?php if($query -> have_posts() ) : ?>
       <?php while($query -> have_posts()) : $query -> the_post();
-          $type = get_post_meta( get_the_ID(), '_rwmwop_type', true);
+        $type = get_post_meta( get_the_ID(), '_rwmwop_type', true);
 
-          $image_size = get_post_meta( get_the_ID(), '_rwmwop_image_size', true );
-          $image_size_width = !empty($image_size['width']) ? $image_size['width'] : null;
-          $image_size_height = !empty($image_size['height']) ? $image_size['height'] : null;
-          $image_size_dimensions = "{$image_size_width}\" x {$image_size_height}\" image size";
+        $image_size = get_post_meta( get_the_ID(), '_rwmwop_image_size', true );
+        $image_size_width = !empty($image_size['width']) ? $image_size['width'] : null;
+        $image_size_height = !empty($image_size['height']) ? $image_size['height'] : null;
+        $image_size_dimensions = "{$image_size_width}\" x {$image_size_height}\" image size";
 
-          $paper_size = get_post_meta( get_the_ID(), '_rwmwop_paper_size', true );
-          $paper_size_width = !empty($paper_size['width']) ? $paper_size['width'] : null;
-          $paper_size_height = !empty($paper_size['height']) ? $paper_size['height'] : null;
-          $paper_size_dimensions = "{$paper_size_width}\" x {$paper_size_height}\" paper size";
+        $paper_size = get_post_meta( get_the_ID(), '_rwmwop_paper_size', true );
+        $paper_size_width = !empty($paper_size['width']) ? $paper_size['width'] : null;
+        $paper_size_height = !empty($paper_size['height']) ? $paper_size['height'] : null;
+        $paper_size_dimensions = "{$paper_size_width}\" x {$paper_size_height}\" paper size";
 
-          $physical_size = get_post_meta( get_the_ID(), '_rwmwop_physical_size', true );
-          $physical_size_width = !empty($physical_size['width']) ? $physical_size['width'] : null;
-          $physical_size_height = !empty($physical_size['height']) ? $physical_size['height'] : null;
-          $physical_size_dimensions = "{$physical_size_width}\" x {$physical_size_height}\" physical size";
+        $physical_size = get_post_meta( get_the_ID(), '_rwmwop_physical_size', true );
+        $physical_size_width = !empty($physical_size['width']) ? $physical_size['width'] : null;
+        $physical_size_height = !empty($physical_size['height']) ? $physical_size['height'] : null;
+        $physical_size_dimensions = "{$physical_size_width}\" x {$physical_size_height}\" physical size";
       ?>
-          <div class='featured-work'>
-            <?php the_post_thumbnail(); ?>
-            <div class='plaque'>
-              <span class='catgory'><?php the_category(' '); ?></span>
-              <h2><?php the_title(); ?></h2>
-              <?php if($type) echo "<h3>{$type}</h3>"; ?>
-              <ul>
-                <?php if($image_size_width && $image_size_height) echo "<li>{$image_size_dimensions}</li>"; ?>
-                <?php if($paper_size_width && $paper_size_height) echo "<li>{$paper_size_dimensions}</li>"; ?>
-                <?php if($physical_size_width && $physical_size_height) echo "<li>{$physical_size_dimensions}</li>"; ?>
-              </ul>
-            </div>
+        <div class='featured-work'>
+          <?php the_post_thumbnail(); ?>
+          <div class='plaque'>
+            <span class='catgory'><?php the_category(' '); ?></span>
+            <h2><?php the_title(); ?></h2>
+            <?php if($type) echo "<h3>{$type}</h3>"; ?>
+            <ul>
+              <?php if($image_size_width && $image_size_height) echo "<li>{$image_size_dimensions}</li>"; ?>
+              <?php if($paper_size_width && $paper_size_height) echo "<li>{$paper_size_dimensions}</li>"; ?>
+              <?php if($physical_size_width && $physical_size_height) echo "<li>{$physical_size_dimensions}</li>"; ?>
+            </ul>
           </div>
+        </div>
       <?php endwhile; ?>
     <?php else : ?>
       <h1>no posts found</h1>
