18fcb1446544a8e692250178b51432c814aabedd
Philip Zak <zakphi@gmail.com>
2018-04-12 16:02:54 -0400

    conditionally render archive posts

--- archive.php
+++ archive.php
@@ -12,19 +12,19 @@
 
   <div id="primary" class="content-area">
     <main id="main" class="site-main">
-
-    <?php
-      $currentCategory = get_category($cat);
-      $currentCategory = $currentCategory -> slug;
-      $query = new WP_Query( array( 'category_name' => $currentCategory ) );
-      while($query -> have_posts()) : $query -> the_post();
-    ?>
-
-      <h1><?php echo $currentCategory ?></h1>
-      <h2><?php the_title(); ?></h2>
-
-    <?php endwhile; ?>
-
+      <?php
+        $currentCategory = get_category($cat);
+        $currentCategory = $currentCategory -> slug;
+        $query = new WP_Query( array( 'category_name' => $currentCategory ) );
+      ?>
+      <?php if($query -> have_posts() ) : ?>
+        <?php while($query -> have_posts()) : $query -> the_post(); ?>
+          <h1><?php echo $currentCategory ?></h1>
+          <h2><?php the_title(); ?></h2>
+        <?php endwhile; ?>
+      <?php else : ?>
+        <h1>no posts found</h1>
+      <?php endif; ?>
     </main><!-- #main -->
   </div><!-- #primary -->
 
