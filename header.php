4e7e44117f31fbae4fc1253af7ae22702efa6692
Philip Zak <zakphi@gmail.com>
2018-05-07 16:52:02 -0400

    remove conditional rendering for site title

--- header.php
+++ header.php
@@ -26,21 +26,13 @@
 
   <header id="masthead" class="site-header">
     <div class="site-branding">
+      <?php the_custom_logo(); ?>
+      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
       <?php
-      the_custom_logo();
-      if ( is_front_page() && is_home() ) :
-        ?>
-        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-        <?php
-      else :
-        ?>
-        <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-        <?php
-      endif;
-      $rwmwop_description = get_bloginfo( 'description', 'display' );
-      if ( $rwmwop_description || is_customize_preview() ) :
-        ?>
-        <p class="site-description"><?php echo $rwmwop_description; /* WPCS: xss ok. */ ?></p>
+        $rwmwop_description = get_bloginfo( 'description', 'display' );
+        if ( $rwmwop_description || is_customize_preview() ) :
+      ?>
+          <p class="site-description"><?php echo $rwmwop_description; /* WPCS: xss ok. */ ?></p>
       <?php endif; ?>
     </div><!-- .site-branding -->
 
