6475fad3184aa60aac0c7941fb0e7fd3941957b8
Philip Zak <zakphi@gmail.com>
2018-06-04 14:25:45 -0400

    set conditional when displaying image so only one image is displayed

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,18 @@ let modal = document.getElementById('modal')
 function showModal(){
   let modalContent = this.parentNode
 
-  modal.style.visibility = 'visible'
-  modal.appendChild(modalContent.cloneNode(true))
+  if(!document.querySelectorAll('#modal .archive-work').length){
+    modal.style.visibility = 'visible'
+    modal.appendChild(modalContent.cloneNode(true))
+  }
 }
 
-let workImg = document.querySelectorAll('.archive .work img')
+let workImg = document.querySelectorAll('.archive-work img')
 workImg.forEach(element => element.onclick = showModal)
 
 function hideModal(){
   modal.style.visibility = 'hidden'
-  modal.removeChild(document.querySelector('#modal .work'))
+  modal.removeChild(document.querySelector('#modal .archive-work'))
 }
 
 let closeBtn = document.getElementById('close-btn')
